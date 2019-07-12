3b33ce9ba58b498011094b0799c5fa66e4af7ddf
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2019-07-12 19:45:41 +0200

    Provide isfastarray

--- src/basics.jl
+++ src/basics.jl
@@ -21,9 +21,30 @@ same as `size(A)` but it is also checked that `A` has standard indices
 function dimensions(A::AbstractArray{<:Any,N}) where {N}
     # The fastest code is LazyAlgebra.has_standard_indexing(A)
     LazyAlgebra.has_standard_indexing(A) ||
-        error("array has non-standard indexing")
+        throw_non_standard_indexing()
     return size(A)
 end
+
+"""
+```julia
+isfastarray()
+```
+
+yields whether array `A` has standard indices and is efficiently indexed by
+linear indices.
+
+"""
+function isfastarray(A::AbstractArray{T,N}) where {T,N}
+    flag = (IndexStyle(A) === IndexLinear())
+    inds = axes(A)
+    @inbounds for d in 1:N
+        flag &= (first(inds[d]) == 1)
+    end
+    return flag
+end
+
+@noinline throw_non_standard_indexing() =
+    error("array have non-standard indices")
 
 #------------------------------------------------------------------------------
 # POINTS AND BOUNDING BOXES
