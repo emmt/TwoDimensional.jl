c09ab52b7928bcc81984ba272c2a42b53f269406
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2024-07-02 18:56:47 +0200

    Update doc. script

--- docs/makedoc.sh
+++ docs/makedoc.sh
@@ -1,2 +1,13 @@
-#! /bin/sh
-exec julia --color=yes make.jl
+#! /bin/bash
+exe=$(realpath "$0")
+dir=$(dirname "$exe")
+cd "$dir/.."
+#pkg=$PWD
+#cd "$dir"
+#echo "{$exe}{$dir}{$pkg}"
+exec julia --color=yes --project="docs/" -e \
+    'using Pkg;
+     Pkg.develop(PackageSpec(path=pwd()));
+     Pkg.instantiate();
+     cd("docs");
+     include("make.jl");'
